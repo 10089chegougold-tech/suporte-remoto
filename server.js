23097a61f249397fd9c801552e687d580fa76008
10089chegougold-tech <10089chegougold@gmail.com>
2026-06-11 21:33:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const wss = new WebSocket.Server({ server });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../painel'));
+app.use(express.static('./'));
 
 // Sessões ativas: { sessionId: { tecnico: ws, cliente: ws, info: {} } }
 const sessions = new Map();
